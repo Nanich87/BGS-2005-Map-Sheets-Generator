d53370a25ff54496ab9661929d1d3ffbe801f520
Nanich87 <flareshadow@gmail.com>
2016-07-27 21:11:31 +0300

    Added Unit Tests

--- CS2005/Data/Point/XYPoint.cs
+++ CS2005/Data/Point/XYPoint.cs
@@ -5,7 +5,8 @@
         private double x;
         private double y;
 
-        public XYPoint() : this(0, 0)
+        public XYPoint()
+            : this(0, 0)
         {
         }
 
@@ -39,6 +40,17 @@
             {
                 this.x = value;
             }
+        }
+
+        public override bool Equals(object obj)
+        {
+            XYPoint other = obj as XYPoint;
+            if (other == null)
+            {
+                return false;
+            }
+
+            return (this.X == other.X && this.Y == other.Y);
         }
     }
 }
